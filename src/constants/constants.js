c884eed8a2d902e1b9ada8d22a5fd5cb258b98a3
Pulkit <297pulkitmehta@gmail.com>
2022-06-06 08:47:05 -0400

    Updated project list

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,43 +1,5 @@
 export const projects = [
   {
-    title: "Dictionary",
-    description:
-      "- Built a dictionary app using reactjs and material UI library",
-    image: "/images/1.jpg",
-    tags: ["React JS", "", "Material UI"],
-    source: "https://github.com/developer-pulkit/Word-hunt-dictionary-app",
-    visit: "https://google.com",
-    id: 0,
-  },
-  {
-    title: "Spice roots recipe world",
-    description:
-      "- Built a food recipe website using reactjs and router library",
-    image: "/images/SpiceRoots.png",
-    tags: ["React", "", "React Router"],
-    source: "https://github.com/297pulkitmehta/RecipeWorld",
-    visit: "https://google.com",
-    id: 1,
-  },
-  {
-    title: "Display Card",
-    description: "- API assignment done using React JS",
-    image: "/images/DisplayCard.png",
-    tags: ["React"],
-    source: "https://github.com/297pulkitmehta/Display-Card",
-    visit: "https://google.com",
-    id: 2,
-  },
-  {
-    title: "Library Tracker",
-    description: "- Built a books tracking app using Vanila Javascript",
-    image: "/images/DisplayCard.png",
-    tags: ["HTML", "CSS", "Javascript"],
-    source: "https://github.com/297pulkitmehta/Display-Card",
-    visit: "https://google.com",
-    id: 3,
-  },
-  {
     title: "Tic-Tac-Too",
     description: "- Built a game using Vanila Javascript",
     image: "/images/DisplayCard.png",
@@ -47,11 +9,52 @@ export const projects = [
     id: 4,
   },
   {
-    title: "Rock Paper Scissors Game",
+    title: "Rock Paper Scissors",
     description: "- Built a Rock Paper Scissors Game using Vanila Javascript",
     image: "/images/DisplayCard.png",
     tags: ["HTML", "CSS", "Javascript"],
-    source: "https://github.com/297pulkitmehta/Tic-Tac-Too",
+    source: "https://github.com/developer-pulkit/Rock-Paper-Scissors-Game",
+    visit: "https://google.com",
+    id: 4,
+  },
+  {
+    title: "Dictionary Application",
+    description: "- Built a dictionary app using React and material UI library",
+    image: "/images/1.jpg",
+    tags: ["React JS", "", "Material UI"],
+    source: "https://github.com/developer-pulkit/Word-hunt-dictionary-app",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "Spice roots recipe world",
+    description:
+      "- Built a food recipe website using React and react-router library",
+    image: "/images/SpiceRoots.png",
+    tags: ["React", "", "React Router"],
+    source: "https://github.com/297pulkitmehta/RecipeWorld",
+    visit: "https://google.com",
+    id: 1,
+  },
+
+  {
+    title: "Netflix clone",
+    description:
+      "- I built a front-end Netflix clone using React Js and tailwind CSS with a Firebase back-end! We use the TMDB API for all of the data AND we will be using Firebase Authentication to give login functionality",
+    image: "/images/DisplayCard.png",
+    tags: ["React", "Tailwind CSS", "Firebase"],
+    source: "https://github.com/developer-pulkit/Netflix-Clone",
+    visit: "https://google.com",
+    id: 4,
+  },
+  {
+    title: "Travel Advisor",
+    description:
+      "- I built a Google Maps Travel Companion Application. Geolocation, Google Maps API & Google Places API, fetching API data based on the location, data filtering are just some features that make this Travel Advisor App the best Maps Application",
+    image: "/images/DisplayCard.png",
+    tags: ["React"],
+    source:
+      "https://github.com/developer-pulkit/Google-Maps-Travel-Companion-Application",
     visit: "https://google.com",
     id: 4,
   },
